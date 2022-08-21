4af8ca7e07d647b68a965f0c811d544ec8147863
Stuart Thomas <stuart.thomas@fred.com.au>
2022-08-21 23:43:03 +1000

    updated image backup script

--- root/usr/local/bin/save_library_images_to_tar.sh
+++ root/usr/local/bin/save_library_images_to_tar.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/with-contenv bash
-. /usr/local/bin/variables
+    . /usr/local/bin/variables
 
-temp_filename_start="library_files_"
+temp_filename_start="library_images"
 
-PLEX_LIBRARY_ROOT="$(dirname "$(dirname "${library_path_local}")")"
-TEMP_TAR_FILE="/tmp/${temp_filename_start}${current_datetime}.tar.gz"
 TAR_BACKUP_FOLDER="${library_images_backup_path_master}"
 LOG_FILE="$TEMP_TAR_FILE.log"
 
-LATEST_TAR_BACKUP_FOLDER_FILE=$(find "$TAR_BACKUP_FOLDER" -name ${temp_filename_start}-20[0-9][0-9]-[0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9].tar.gz | sort | tail -n 1)
+LATEST_TAR_BACKUP_FOLDER_FILE=$(find "$TAR_BACKUP_FOLDER" -name ${temp_filename_start}_[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9]_to_[0-9][0-9][0-9][0-9]-20[0-9][0-9]-[0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9].tar.gz | sort | tail -n 1)
 LATEST_TAR_BACKUP_FOLDER_FILE=$(basename "$LATEST_TAR_BACKUP_FOLDER_FILE")
-LATEST_TAR_BACKUP_DATE="TBA"
+LATEST_TAR_BACKUP_DATE=$(echo $LATEST_TAR_BACKUP_FOLDER_FILE | cut -d "." -f1 | cut -d "_" -f5)
+LATEST_TAR_BACKUP_DATE=${LATEST_TAR_BACKUP_DATE:-"0000-00-00-0000"}
+echo "Latest Image Backup TAR: ${LATEST_TAR_BACKUP_FOLDER_FILE}"
+
+TEMP_TAR_FILE="/tmp/${temp_filename_start}_${LATEST_TAR_BACKUP_DATE}_to_${current_datetime}.tar.gz"
+echo "Temp Image Backup TAR: ${TEMP_TAR_FILE}"
 
 echo "$(date) ****** Starting image Libary tar file rebuild ******"
 echo "$(date) ****** Starting image Libary tar file rebuild ******" > "$LOG_FILE"
-cd "$PLEX_LIBRARY_ROOT"
+cd "$library_root_path_local"
 tar -cvpzf "$TEMP_TAR_FILE" "./Application Support/Plex Media Server/Metadata" "./Application Support/Plex Media Server/Media" >> "$LOG_FILE"
 
 if [ $(gzip -t "$TEMP_TAR_FILE") ]
