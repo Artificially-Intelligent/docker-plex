65cee0c06b4adbfcf9816fc4cfdd89c691956830
Stuart Thomas <stuart.thomas@fred.com.au>
2022-10-18 00:44:10 +1100

    inital test version of image load sh

--- root/usr/local/bin/load_library_images_from_tar.sh
+++ root/usr/local/bin/load_library_images_from_tar.sh
@@ -8,40 +8,33 @@ LOG_FILE="${library_path_local}/library_images.log"
 DONE_FILE="${library_path_local}/library_images_loaded.dat"
 FOUND_FILE="${library_path_local}/library_images_found.dat"
 
-# [ -f "${DONE_FILE}" ] && loaded_tar_backup_files=$(cat ${DONE_FILE})
+[ -f "${DONE_FILE}" ] && loaded_tar_backup_files=$(cat ${DONE_FILE}) || 
 
-find "$TAR_BACKUP_FOLDER" -name find "$TAR_BACKUP_FOLDER" -name "${library_images_tar_filename_start}_[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] [0-9][0-9][0-9][0-9]_to_[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] [0-9][0-9][0-9][0-9]_[0-9][0-9][0-9].tar.gz" | sort > ${FOUND_FILE}
+find "$TAR_BACKUP_FOLDER" -name "${library_images_tar_filename_start}_[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] [0-9][0-9][0-9][0-9]_to_[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] [0-9][0-9][0-9][0-9]_[0-9][0-9][0-9].tar.gz" | sort > "${FOUND_FILE}"
 
-echo "$(date) ****** Starting image Libary load from tar file ******"
+echo "$(date) ****** Starting image Libary load from tar $(cat "${FOUND_FILE}" | wc -l) file(s) ******"
 
 cd "${library_path_local}"
 while read tar_backup_file;
 do 
-    tar_backup_filename=$(basename ${tar_backup_file})
-    if [ $(cat ${DONE_FILE} | grep ${tar_backup_filename}) ]
+    tar_backup_filename=$(basename "${tar_backup_file}")
+    if [[ $(cat "${DONE_FILE}" | grep "${tar_backup_filename}") ]]
     then
-        echo "skipping and deleted tar found in done file {$(basename ${DONE_FILE})}: ${tar_backup_file}"
+        echo "skipping and deleted tar found in done file ($DONE_FILE): \"${tar_backup_file}\""
         rm "${tar_backup_file}"
     else
-        echo "Loading images from Image Backup TAR: $tar_backup_file"
-
-        echo "${tar_backup_file}" >>  "${DONE_FILE}"
+        if [ $(gzip -t "${tar_backup_file}") ]
+        then
+            echo "Loading images from Image Backup TAR: $tar_backup_file"
+            #tar -xzf "$tar_backup_file" -C "$library_path_local" --checkpoint=.5000
+            # formatted to match rclone --filter-from requirements
+            echo "- ${tar_backup_filename}" >>  "${DONE_FILE}"
+        else
+            echo "error tar gzip compression failed when tested, removing file from disk ahead of fresh download from cloud storage: ${tar_backup_file}"
+            rm "${tar_backup_file}"
+        fi
     fi
-done < ${FOUND_FILE}
+done < "${FOUND_FILE}"
 
 echo "$(date) ****** Finished image Libary load from tar file ******"
-
 echo "$(date) ****** Starting image Libary load from tar file ******" > "$LOG_FILE"
-cd "${library_path_local}"
-
-if [ $(gzip -t "$LATEST_TAR_BACKUP_FOLDER_FILE") ]
-then
-    echo "tar gzip compression tested ok, overwriting old version"
-    tar -cvpzf "$TEMP_TAR_FILE" "./Metadata" "./Media" >> "$LOG_FILE"
-else
-    echo "error tar gzip compression failed when tested, removing file from disk: ${LATEST_TAR_BACKUP_FOLDER_FILE}"
-    rm "${LATEST_TAR_BACKUP_FOLDER_FILE}"
-fi
-
-echo "$(date) ****** Finished image Libary tar file rebuild ******" >> "$LOG_FILE"
-echo "$(date) ****** Finished image Libary tar file rebuild ******"
